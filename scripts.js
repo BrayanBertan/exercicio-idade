5e85c15a016e4c8e34b1d64332055c44e6645ce2
BrayanBertan <brayanbertan@gmail.com>
2020-09-21 20:56:13 -0300

    mudar nome dos campos para algo que faça mais sentido

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
+var nasc = document.getElementById('nasc');
 var idade = document.getElementById('idade');
-var resultado = document.getElementById('resultado');
 document.getElementById('calcularIdade').addEventListener('click',calcular);
 document.getElementById('limpar').addEventListener('click',limpar);
 
 function calcular() {
-    resultado.innerText = 'Idade: ' +  (new Date().getFullYear() - parseInt(idade.value.trim())) + ' ano(s).';
+    idade.innerText = 'Idade: ' +  (new Date().getFullYear() - parseInt(nasc.value.trim())) + ' ano(s).';
 }
 
 function limpar() {
-    idade.value = '';
-    resultado.innerText = '';
+    nasc.value = '';
+    idade.innerText = '';
 }
 
